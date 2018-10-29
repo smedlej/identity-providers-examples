93ab9834160ac4b1620139ce80d6baa5dbddac1c
SandyCT97 <sandy.pierre@consertotech.pro>
2018-10-29 16:04:11 +0100

    Modify database.js

--- NodeJSES6/data/database.js
+++ NodeJSES6/data/database.js
@@ -3,19 +3,7 @@ import csvdb from 'node-csv-query';
 
 const database = { connection: null };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 csvdb(path.join(__dirname, './database.csv'), { rtrim: true }).then((db) => {
-||||||| parent of 14105d7... Modify DB path
-csvdb(path.join(__dirname, '../data/database.csv'), { rtrim: true }).then((db) => {
-=======
-csvdb(path.join(__dirname, '../../../fcp_mocks_dataset/fcp_mock_dataset.csv'), { rtrim: true }).then((db) => {
->>>>>>> 14105d7... Modify DB path
-||||||| parent of ac4a42b... Modify DB path in database.js
-csvdb(path.join(__dirname, '../../../fcp_mocks_dataset/fcp_mock_dataset.csv'), { rtrim: true }).then((db) => {
-=======
-csvdb(path.join(__dirname, './database.csv'), { rtrim: true }).then((db) => {
->>>>>>> ac4a42b... Modify DB path in database.js
   // eslint-disable-next-line no-console
   console.log('Connected to database!');
 
