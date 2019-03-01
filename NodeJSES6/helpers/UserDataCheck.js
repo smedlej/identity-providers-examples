f18f7bb92f568d15256b2a34c1a7244fcbe8a945
valentin bonnard <vbonnard@kaliopmail.com>
2019-03-01 11:34:37 +0100

    [FCA] Add proper userinfo to FCA

--- NodeJSES6/helpers/UserDataCheck.js
+++ NodeJSES6/helpers/UserDataCheck.js
@@ -2,149 +2,7 @@
 /* eslint-disable class-methods-use-this */
 class UserDataCheck {
   checkMandatoryData(user) {
-    let userInfo;
-    if (
-      user[0].nomDeNaissance === ''
-      || user[0].nomDeNaissance === undefined
-      || user[0].nomDeNaissance === null
-      || typeof user[0].nomDeNaissance !== 'string'
-    ) {
-      userInfo = {
-        address: user[0].adresseFormatee,
-        birthdate: user[0].birthdate,
-        birthcountry: user[0].codePaysDeNaissance,
-        birthplace: user[0].codeCommune,
-        email: user[0].email,
-        gender: user[0].Gender,
-        given_name: user[0].prenom,
-        middle_name: user[0].secondPrenom,
-        nickname: user[0].prenom,
-        phone: user[0].telephone,
-        preferred_username: user[0].prenom,
-        updated_at: user[0].updatedAt,
-        siren: user[0].siren,
-        siret: user[0].siret,
-        organizational_unit: user[0].serviceAffectation,
-        belonging_population: user[0].populationAppartenance,
-        position: user[0].position,
-        job: user[0].job,
-        uid: user[0].uid
-      };
-    } else if (
-      user[0].prenom === ''
-      || user[0].prenom === undefined
-      || user[0].prenom === null
-      || typeof user[0].prenom !== 'string'
-    ) {
-      userInfo = {
-       address: user[0].adresseFormatee,
-        birthdate: user[0].birthdate,
-        birthcountry: user[0].codePaysDeNaissance,
-        birthplace: user[0].codeCommune,
-        email: user[0].email,
-        family_name: user[0].nomDeNaissance,
-        usual_name: user[0].nomDeNaissance,
-        gender: user[0].Gender,
-        middle_name: user[0].secondPrenom,
-        phone: user[0].telephone,
-        updated_at: user[0].updatedAt,
-        siren: user[0].siren,
-        siret: user[0].siret,
-        organizational_unit: user[0].serviceAffectation,
-        belonging_population: user[0].populationAppartenance,
-        position: user[0].position,
-        job: user[0].job,
-        uid: user[0].uid
-      };
-    } else if (
-      user[0].birthdate === ''
-      || user[0].birthdate === undefined
-      || user[0].birthdate === null
-      || typeof user[0].birthdate !== 'string'
-    ) {
-      userInfo = {
-        address: user[0].adresseFormatee,
-        birthcountry: user[0].codePaysDeNaissance,
-        birthplace: user[0].codeCommune,
-        email: user[0].email,
-        family_name: user[0].nomDeNaissance,
-        usual_name: user[0].nomDeNaissance,
-        gender: user[0].Gender,
-        given_name: user[0].prenom,
-        middle_name: user[0].secondPrenom,
-        name: `${user[0].nomDeNaissance} ${user[0].prenom}`,
-        nickname: user[0].prenom,
-        phone: user[0].telephone,
-        preferred_username: `${user[0].prenom}_${user[0].nomDeNaissance}`,
-        updated_at: user[0].updatedAt,
-        siren: user[0].siren,
-        siret: user[0].siret,
-        organizational_unit: user[0].serviceAffectation,
-        belonging_population: user[0].populationAppartenance,
-        position: user[0].position,
-        job: user[0].job,
-        uid: user[0].uid
-      };
-    } else if (
-      user[0].codeCommune === ''
-      || user[0].codeCommune === undefined
-      || user[0].codeCommune === null
-      || typeof user[0].codeCommune !== 'string'
-    ) {
-      userInfo = {
-        address: user[0].adresseFormatee,
-        birthdate: user[0].birthdate,
-        birthcountry: user[0].codePaysDeNaissance,
-        email: user[0].email,
-        family_name: user[0].nomDeNaissance,
-        usual_name: user[0].nomDeNaissance,
-        gender: user[0].Gender,
-        given_name: user[0].prenom,
-        middle_name: user[0].secondPrenom,
-        name: `${user[0].nomDeNaissance} ${user[0].prenom}`,
-        nickname: user[0].prenom,
-        phone: user[0].telephone,
-        preferred_username: `${user[0].prenom}_${user[0].nomDeNaissance}`,
-        updated_at: user[0].updatedAt,
-        siren: user[0].siren,
-        siret: user[0].siret,
-        organizational_unit: user[0].serviceAffectation,
-        belonging_population: user[0].populationAppartenance,
-        position: user[0].position,
-        job: user[0].job,
-        uid: user[0].uid
-      };
-    } else if (
-      user[0].Gender === ''
-      || user[0].Gender === undefined
-      || user[0].Gender === null
-      || typeof user[0].Gender !== 'string'
-    ) {
-      userInfo = {
-        address: user[0].adresseFormatee,
-        birthdate: user[0].birthdate,
-        birthcountry: user[0].codePaysDeNaissance,
-        birthplace: user[0].codeCommune,
-        email: user[0].email,
-        family_name: user[0].nomDeNaissance,
-        usual_name: user[0].nomDeNaissance,
-        given_name: user[0].prenom,
-        middle_name: user[0].secondPrenom,
-        name: `${user[0].nomDeNaissance} ${user[0].prenom}`,
-        nickname: user[0].prenom,
-        phone: user[0].telephone,
-        preferred_username: `${user[0].prenom}_${user[0].nomDeNaissance}`,
-        updated_at: user[0].updatedAt,
-        siren: user[0].siren,
-        siret: user[0].siret,
-        organizational_unit: user[0].serviceAffectation,
-        belonging_population: user[0].populationAppartenance,
-        position: user[0].position,
-        job: user[0].job,
-        uid: user[0].uid
-      };
-    } else {
-      userInfo = {
+    let userInfo = {
         address: user[0].adresseFormatee,
         birthdate: user[0].birthdate,
         birthcountry: user[0].codePaysDeNaissance,
@@ -158,7 +16,7 @@ class UserDataCheck {
         name: `${user[0].nomDeNaissance} ${user[0].prenom}`,
         nickname: user[0].prenom,
         phone: user[0].telephone,
-        preferred_username: `${user[0].prenom}_${user[0].nomDeNaissance}`,
+        preferred_username: user[0].identifiant,
         updated_at: user[0].updatedAt,
         siren: user[0].siren,
         siret: user[0].siret,
@@ -167,7 +25,6 @@ class UserDataCheck {
         position: user[0].position,
         job: user[0].job,
         uid: user[0].uid
-      };
     }
 
     return userInfo;
